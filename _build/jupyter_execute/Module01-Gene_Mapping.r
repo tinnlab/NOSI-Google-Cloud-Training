354a68fbb847686961c73e869929ab2c628cb875
Tran Sy Bang <bangtran365@gmail.com>
2022-07-26 14:14:00 -0700

    Bang
    fix error in Biomanager

--- _build/jupyter_execute/Module01-Gene_Mapping.r
+++ _build/jupyter_execute/Module01-Gene_Mapping.r
@@ -1,3 +1,18 @@
 #if (!require("BiocManager", quietly = TRUE)
 #   install.packages("BiocManager")
 BiocManager::install("hgu133plus2.db", update = F)
+
+data = readRDS("./data/GSE48350.rds")
+probeIDs = rownames(data)
+probeIDs
+
+library(hgu133plus2.db)
+
+annotLookup <- select(hgu133plus2.db, keys = probeIDs, columns = c('PROBEID', 'ENSEMBL', 'SYMBOL'))
+
+annotLookup
+
+rownames(data) <- unique(annotLookup$SYMBOL)
+
+data
+
